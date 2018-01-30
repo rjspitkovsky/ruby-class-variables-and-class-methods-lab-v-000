45341de3b55d255fdaac5e0fb13a8f66b5a48b06
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-30 21:45:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,17 @@
 class Song
 
-attr_reader :name, :artist, :genre 
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+attr_reader :name, :artist, :genre
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre 
   end
 
 
